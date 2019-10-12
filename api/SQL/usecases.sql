e14710bda696af1d8da3215ad4ff7ab5d103b0d3
jamesverley <dyseterboy@gmail.com>
2019-10-12 14:40:15 +1100

    add use cases for sql

---
+++ api/SQL/usecases.sql
@@ -0,0 +1,21 @@
+-- =============================
+
+-- teacher able to get all:
+    -- the subject_periods given a particular subject
+    -- subjects they are assigned to
+    -- subjects for a particular year level
+    -- criteria for a particular subject
+    -- sessions taught so far
+    -- grades given a particular session
+    -- students for a given yearlevel
+    -- students for a given subject
+    -- yearlevels for a given year
+    -- other teachers firstname, lastname and email
+
+-- teacher able to add, update and remove:
+    -- subject records (deleting also deletes subject records)
+    -- criteria to subject records
+    -- session records (deleting will also remove all relevant grade records)
+    -- grade records
+    -- year level records
+    -- student records
