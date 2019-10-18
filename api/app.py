37def1353221d8f49f8bf5c2314020f291aaabfa
juntao-dev <juntao.dev@gmail.com>
2019-10-19 02:30:13 +1100

    chore/heroku setup fix

--- api/app.py
+++ api/app.py
@@ -176,4 +176,5 @@ def catch_all(path):
     return jsonify({"message":"path not found"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+		app.run(host='0.0.0.0', port=80)
+    # app.run(debug=True)
